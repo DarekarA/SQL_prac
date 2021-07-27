fc8f86f38effc440fba44057ef649d88b23ccfca
DarekarA <darekarabhishek@gmail.com>
2021-07-27 23:11:24 +0530

    Add files via upload

---
+++ Topic Names.sql
@@ -0,0 +1,64 @@
+# DAY 1
+
+/*
+1.  SELECT STATEMENTS
+2. Sorting data
+
+*/
+
+# DAY 2
+
+/*
+1.  WHERE CLAUSE
+	1. AND
+	2. OR
+	3. LIKE
+	4. IN 
+	5. BETWEEN 
+	6. LIMIT
+	7. IS NULL 
+    
+2. DISTINCT CLAUSE
+3. Creating Database
+4. Creating Table
+
+
+*/
+
+# DAY 3
+
+/*
+1.  Insert Staement
+2. DML Statement (ALTER, UPDATE, DELETE)
+3. Group by 
+4. HAVING 
+5. Alias name
+7. sql joins 
+9. subquery
+
+*/
+
+
+# DAY 4
+
+/*
+1.  constraint
+	1. NOT NULL constraint
+	2. Primary Key
+	3. Foreign key
+	4. UNIQUE constraint
+	5. CHECK Constraint
+2. STORED PROCEDURE
+3. if else
+4. case statement
+5. while
+7. loop
+8. functions
+9. trrigers
+10. views 
+
+END
+*/
+
+
+
